2d97d3234ed9f4d1d3f952ae7c0bf4d4a8f79dca
Gnimuc <qiyupei@gmail.com>
2019-01-19 19:27:06 +0800

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,3 +3,8 @@ using Test
 
 @cenum(Fruit, apple=1, orange=2, kiwi=2)
 @test orange == kiwi
+@test apple | orange == 3
+@test apple & orange == 0
+
+@cenum(Boolean{Bool}, alternativefact, fact)
+@test alternativefact == false
