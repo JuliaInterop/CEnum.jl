1e0b55869bc2872c6bdb8be729d37cdbfdceefbf
Gnimuc <qiyupei@gmail.com>
2019-07-22 22:21:07 +0800

    Allow non-member integers to be casted to enums

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,6 +12,7 @@ using Test
 @test apple + 1 == 2
 @test kiwi - 1 == 1
 @test kiwi ⊻ kiwi == 0
+@test_nowarn print(Fruit(apple | orange))
 
 @cenum(Boolean::Bool, alternativefact, fact)
 @test alternativefact == false
