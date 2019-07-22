b74663cd17ef12f19391dda96d5347e13450c040
Gnimuc <qiyupei@gmail.com>
2019-07-22 21:41:13 +0800

    C99 compliance

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,19 @@
 using CEnum
 using Test
 
+@cenum(ZeroOne, zero, one)
+@test zero == 0
+@test one == 1
+
 @cenum(Fruit, apple=1, orange=2, kiwi=2)
 @test orange == kiwi
 @test apple | orange == 3
 @test apple & orange == 0
 @test apple + 1 == 2
 @test kiwi - 1 == 1
+@test kiwi ⊻ kiwi == 0
 
-@cenum(Boolean{Bool}, alternativefact, fact)
+@cenum(Boolean::Bool, alternativefact, fact)
 @test alternativefact == false
 
 @cenum(Day, Mon=1, Tue, Wed=3, suiyoubi=3, Fri=5, Sat)
